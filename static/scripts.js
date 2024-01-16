7f94158cabbb6999c76e6d75e00c6549e44e3d9c
Michael Spencer <mikespencer76@gmail.com>
2024-01-16 08:43:30 -0400

    added javascript file

---
+++ static/scripts.js
@@ -0,0 +1,11 @@
+//function to convert UTC time to users local time zone
+function convertTime (data, num) {
+
+    var utc = data.gameWeek[0]['games'][num]['startTimeUTC'];
+    var date = new Date(utc);
+    var local_time = new Date(date.toString());
+    var start24 = local_time.getHours() + ":" + local_time.getMinutes() + "0";
+
+    return start24;
+    
+}
