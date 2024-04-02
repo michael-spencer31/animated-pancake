0b1c2cd7f21471e3d0592e8ff7f4345aca3a6396
Michael Spencer <mikespencer76@gmail.com>
2024-04-02 13:23:20 -0300

    continued to work on player page. added a few new functions

--- static/scripts.js
+++ static/scripts.js
@@ -17,3 +17,56 @@ function getGoalie (goalie) {
 function getSchedule (response, past_day) {
     console.log("Hello, World!");
 }
+
+function overallPick (round, pick) {
+
+    if (round == 1) {
+        return getNumberWithOrdinal(pick);
+    } else {
+        return getNumberWithOrdinal((round - 1) * 32 + pick);
+    }
+}
+
+function getNumberWithOrdinal (n) {
+
+    var s = ["th", "st", "nd", "rd"], v = n % 100;
+
+    return n + (s[(v - 20) % 10] || s[v] || s[0]);
+}
+function convertAbrv(team) {
+
+    const team_names = new Map([
+        ["COL", "Colorado"],
+        ["MTL", "Montréal"],
+        ["BOS", "Boston"],
+        ["CAR", "Carolina"],
+        ["CBJ", "Columbus"],
+        ["TOR", "Toronto"],
+        ["CGY", "Calgary"],
+        ["LAK", "Los Angeles"],
+        ["EDM", "Edmonton"],
+        ["DAL", "Dallas"],
+        ["DET", "Detriot"],
+        ["MIN", "Minnesota"],
+        ["NJD", "New Jersey"],
+        ["STL", "St. Louis"],
+        ["SJS", "San Jose"],
+        ["VAN", "Vancouver"],
+        ["TBL", "Tampa Bay"],
+        ["WIN", "Winnipeg"],
+        ["NSH", "Nashville"],
+        ["WSH", "Washington"],
+        ["SEA", "Seattle"],
+        ["VGK", "Vegas"],
+        ["PHI", "Philadelphia"],
+        ["NYR", "Rangers"],
+        ["NYI", "Islanders"],
+        ["ATL", "Atlanta"],
+        ["BUF", "Buffalo"],
+        ["CHI", "Chicago"],
+        ["PIT", "Pittsburgh"],
+        ["ARI", "Arizona"],
+        ["OTT", "Ottawa"],
+    ]);
+    return team_names.get(team);
+}
